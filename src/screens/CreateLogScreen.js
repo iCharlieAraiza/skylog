021ac3832c44c6fa309f77d6e862e89be9029ef1
iCharlieAraiza <eliptikman1234@gmail.com>
2022-12-09 17:55:08 -0600

    Init app

---
+++ src/screens/CreateLogScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export function CreateLogScreen() {
+  return (
+    <View>
+      <Text>CreateLog</Text>
+    </View>
+  )
+}
