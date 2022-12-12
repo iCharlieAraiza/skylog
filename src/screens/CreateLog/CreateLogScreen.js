276b0a98c457d3c6c7f7567969d997fcea26b484
iCharlieAraiza <eliptikman1234@gmail.com>
2022-12-11 21:41:33 -0600

    Create ReadME

--- src/screens/CreateLog/CreateLogScreen.js
+++ src/screens/CreateLog/CreateLogScreen.js
@@ -8,14 +8,33 @@ import { screen } from '../../utils/screenName'
 import { InfoForm } from '../../components/CreateLog/InfoForm/InfoForm';
 import { initialVales, validationSchema } from './CreateLogScreen.data';
 import { useFormik } from 'formik';
+import { v4 as uuid } from "uuid";
+
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../../utils/firebase";
+import { useNavigation } from "@react-navigation/native";
+
 
 export function CreateLogScreen() {
+  const navigation = useNavigation();
+
   const formik = useFormik({
     initialValues: initialVales(),
     validationSchema: validationSchema(),
     validateOnChange: false,
     onSubmit: async (formData) => {
-      console.log("Hola", formData);
+      try {
+        const newData = formData
+        newData.id = uuid()
+        newData.createdAt = new Date()
+        await setDoc(doc(db, "flights", newData.id), newData);
+        navigation.goBack();
+
+        console.log(newData)
+      } catch (error) {
+        console.log(error);
+      }
+      console.log("Hola", formData)
     }
   });
   return (
--- src/screens/Flights/FlightsScreen.js
+++ src/screens/Flights/FlightsScreen.js
@@ -1,24 +1,41 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import { Button } from '@rneui/base'
-import { useNavigation } from '@react-navigation/native'
-import { screen } from '../../utils/screenName'
-
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import { Button } from "@rneui/base";
+import { useNavigation } from "@react-navigation/native";
+import { screen } from "../../utils/screenName";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../../utils/firebase";
+import { LoadingModal } from "../../components/shared/LoadingModal/LoadingModal.js";
 
 export function FlightsScreen(props) {
-
-  const { navigation } = props
+  const { navigation } = props;
+  const [flights, setFlights] = useState(null);
 
   const handlePress = () => {
     //navigation.navigate(screen.flights.showFlight)
-    navigation.navigate(screen.createLog.tab, { screen: screen.createLog.create })
-    console.log('Ver vuelo', navigation)
-  }
+    navigation.navigate(screen.createLog.tab, {
+      screen: screen.createLog.create,
+    });
+    console.log("Ver vuelo", navigation);
+  };
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "flights"),
+      orderBy("createdAt", "desc")
+    );
+
+    onSnapshot(q, (snapshot) => {
+      console.log(snapshot.docs)
+      setFlights(snapshot.docs);
+    });
+  }, []);
+
 
   return (
     <View>
       <Text>Flights</Text>
-      <Button title='Ver vuelo' onPress={handlePress} />
+      <Button title="Ver vuelo" onPress={handlePress} />
     </View>
-  )
+  );
 }
